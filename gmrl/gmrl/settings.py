4aff82458a877e715efa28d91e93d2aefadf797c
amaljithkaliyodath <amaljithkaliyodath1@gmail.com>
2024-01-05 16:08:23 +0530

    add

--- gmrl/gmrl/settings.py
+++ gmrl/gmrl/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -120,8 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-# STATICFILES_DIRS =[os.path(BASE_DIR,'STATIC'),]
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS =[os.path.join(BASE_DIR,'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
